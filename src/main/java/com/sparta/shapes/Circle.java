b5705eb70cee092e377e9dd4f5e47cc584313ebb
MustyC <mustaphachedid@gmail.com>
2022-05-05 17:00:27 +0100

    Added failing unit test. Implemented interface and resolved compile errors.

--- src/main/java/com/sparta/shapes/Circle.java
+++ src/main/java/com/sparta/shapes/Circle.java
@@ -1,4 +1,16 @@
 package com.sparta.shapes;
 
-public class Circle {
+import java.math.BigDecimal;
+
+public class Circle implements Shape{
+  private int radius;
+
+  public Circle( int radius ) {
+    this.radius = radius;
+  }
+
+  @Override
+  public BigDecimal getSurfaceArea() {
+    return new BigDecimal( "0" );
+  }
 }
--- src/test/java/com/sparta/shapes/CircleTest.java
+++ src/test/java/com/sparta/shapes/CircleTest.java
@@ -1,4 +1,15 @@
 package com.sparta.shapes;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 public class CircleTest {
+  @Test
+  public void surfaceAreaOfACircle() {
+    Circle circle = new Circle(15);
+    BigDecimal actualSurfaceArea = circle.getSurfaceArea();
+    Assertions.assertEquals( new BigDecimal( "706.5" ), actualSurfaceArea );
+  }
 }
