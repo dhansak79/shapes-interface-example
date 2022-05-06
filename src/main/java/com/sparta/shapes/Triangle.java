98477d235c39b44265aa1d823239aa15b9c9f988
Ahmad <amadon2g@yahoo.com>
2022-05-06 10:47:23 +0100

    Triangle class created and tests passed

---
+++ src/main/java/com/sparta/shapes/Triangle.java
@@ -0,0 +1,19 @@
+package com.sparta.shapes;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Triangle implements Shape{
+  private final double base;
+  private final double height;
+
+  public Triangle(double base, double height) {
+    this.base = base;
+    this.height = height;
+  }
+  @Override
+  public BigDecimal getSurfaceArea() {
+    return BigDecimal.valueOf( base ).multiply( BigDecimal.valueOf( height ) ).multiply( new BigDecimal( "0.5" ) )
+            .setScale( 2, RoundingMode.HALF_UP  );
+  }
+}
